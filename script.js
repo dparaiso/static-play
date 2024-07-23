8856c6bb1141b7d329830ccf553d11a196420aa9
dparaiso <danieva.paraiso@gmail.com>
2024-07-23 12:22:16 -0700

    growing dot start

--- script.js
+++ script.js
@@ -1,58 +1,42 @@
-const galleryContainer = document.querySelector('.gallery-container'); 
-const galleryControlsContainer = document.querySelector('.gallery-controls'); 
-const galleryControls = ['previous', 'next']; 
-const galleryItems = document.querySelectorAll('.gallery-item');
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-class Carousel {
 
-    constructor(container, items, controls){
-        this.carouselContainer = container; 
-        this.carouselControls = controls; 
-        this.carouselArray = [...items]; 
+/*------------------------------
+Register plugins
+------------------------------*/
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+
+/*------------------------------
+Init ScrollSmoother
+------------------------------*/
+const scrollerSmoother = ScrollSmoother.create({
+  content: '#content',
+  wrapper: '#wrapper',
+  smooth: true,
+  effects: false,
+  normalizeScroll: true
+})
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+      trigger: '.accordions',
+      pin: true,
+      start: 'top top',
+      end: 'bottom top',
+      scrub: 1,
+      ease: 'linear',
     }
+})
 
-    updateGallery(){
-        this.carouselArray.forEach(el => {
-            el.classList.remove('gallery-item-1');
-            el.classList.remove('gallery-item-2');
-            el.classList.remove('gallery-item-3');
-            el.classList.remove('gallery-item-4');
-            el.classList.remove('gallery-item-5');
-        });
-
-        this.carouselArray.slice(0, 5).forEach((el, i) => {
-            el.classList.add(`gallery-item-${i+1}`);
-        });
-    }
-
-    setCurrentState(direction){
-        if (direction.className == 'gallery-controls-previous'){
-            this.carouselArray.unshift(this.carouselArray.pop()); 
-        }else{
-            this.carouselArray.push(this.carouselArray.shift()); 
-        }
-        this.updateGallery(); 
-    }
-
-    setControls() {
-        this.carouselControls.forEach(control => {
-            galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
-            document.querySelector(`.gallery-controls-${control}`).innerText = control; 
-        });
-    }
-
-    useControls(){
-        const triggers = [...galleryControlsContainer.childNodes]; 
-        triggers.forEach(control => {
-            control.addEventListener('click', e => {
-                e.preventDefault(); 
-                this.setCurrentState(control); 
-            });
-        });
-    }
-}
-
-const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls); 
-
-exampleCarousel.setControls(); 
-exampleCarousel.useControls(); 
+tl.to('.accordion .text', {
+  height: 0,
+  paddingBottom: 0,
+  opacity: 0,
+  stagger: .5,
+})
+tl.to('.accordion', {
+  marginBottom: -15,
+  stagger: .5,
+}, '<')
